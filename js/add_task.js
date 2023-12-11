9959cca21415abf89fd04ca7be8cc947a11c623a
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2023-12-11 17:41:13 +0100

    cleaned code

--- js/add_task.js
+++ js/add_task.js
@@ -1,30 +1,51 @@
+/** Funktion bestimmt, was bei Klick auf einen der drei Prioritätsbuttons geschieht 
+ * 
+ * @param {number} btnNumber - Laufindex des geklickten Buttons (1: urgent, 2: medium, 3: low) 
+ */
 function handlePrioBtnClick(btnNumber) {
     for (let i = 1; i <= 3; i++) {
-        const btn = document.getElementById('addTaskPrio' + i);
-        if (i == btnNumber) { // falls dieser Button geklickt wurde
-            btn.classList.toggle('addTaskPrioBtnsSelected');
-            btn.classList.toggle(`addTaskPrio${i}Selected`);
-            togglePrioBtnImg(i);
-        } else { // CSS-Klassen entfernen, falls anderer Button geklickt wurde
-            btn.classList.remove('addTaskPrioBtnsSelected');
-            btn.classList.remove(`addTaskPrio${i}Selected`);
-            colorPrioBtnImg(i);
-        }
+        stylePrioBtn(i, btnNumber);
     }
 }
 
-function togglePrioBtnImg(i) {
-    const img = document.getElementById(`addTaskPrio${i}Img`);
+/** Button stylen
+ * 
+ * @param {number} index - Laufindex des zu stylenden Buttons
+ * @param {*} btnNumber - Laufindex des geklickten Buttons
+ */
+function stylePrioBtn(index, btnNumber) {
+    const btn = document.getElementById('addTaskPrio' + index);
+    if (index == btnNumber) { // falls dieser Button geklickt wurde
+        btn.classList.toggle('addTaskPrioBtnsSelected');
+        btn.classList.toggle(`addTaskPrio${index}Selected`);
+        togglePrioBtnImg(index);
+    } else { // CSS-Klassen entfernen, falls anderer Button geklickt wurde
+        btn.classList.remove('addTaskPrioBtnsSelected');
+        btn.classList.remove(`addTaskPrio${index}Selected`);
+        colorPrioBtnImg(index);
+    }
+}
+
+/** <img> im Button durch Pfadänderung stylen
+ * 
+ * @param {number} index - Laufindex des Buttons 
+ */
+function togglePrioBtnImg(index) {
+    const img = document.getElementById(`addTaskPrio${index}Img`);
     if (img.src.includes('white')) {
-        colorPrioBtnImg(i);
+        colorPrioBtnImg(index);
     } else {
         let newSrc = img.src.replace('.svg', '_white.svg');
         img.src = newSrc;
     }
 }
 
-function colorPrioBtnImg(i) {
-    const img = document.getElementById(`addTaskPrio${i}Img`);
+/** <img> im Button bunt färben 
+ * 
+ * @param {number} index - Laufindex des Buttons 
+ */
+function colorPrioBtnImg(index) {
+    const img = document.getElementById(`addTaskPrio${index}Img`);
     if (img.src.includes('white')) {
         let newSrc = img.src.replace('_white', '');
         img.src = newSrc;
