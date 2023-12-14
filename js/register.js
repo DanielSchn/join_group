9ac0ed01cee18ecfe9fe3d41a366a96ff69af8f4
DanielSchn <d.schneider@dsc-mail>
2023-12-14 12:26:55 +0100

    sign up function

--- js/register.js
+++ js/register.js
@@ -1,36 +1,63 @@
-/**
- * Referenzen zu den DOM-Elementen
- */
-document.addEventListener('DOMContentLoaded', function () {
-    var signUpButton = document.querySelector('.signUpButton');
-    var checkbox = document.querySelector('.acceptPrivacy input[type="checkbox"]');
-    var passwordInput = document.querySelector('input[placeholder="Password"]');
-    var confirmPasswordInput = document.querySelector('input[placeholder="Confirm Password"]');
-    var errorMessage = document.querySelector('.error-message');
+let users = [];
+
+
+async function init(){
+     loadUsers();
+ }
+
 
 /**
- * Funktion, um den Anmelde-Button zu aktivieren/deaktivieren und Fehlermeldungen anzuzeigen
+ * This is the function to load the Data from the remot storage and convert it into a JSON Array
+ * If there are no Data we get an error log into the console
  */
-    function toggleSignUpButton() {
-        var passwordsMatch = passwordInput.value === confirmPasswordInput.value;
-        var isCheckboxChecked = checkbox.checked;
-
-        signUpButton.disabled = !isCheckboxChecked || !passwordsMatch;
-
-        // Anzeige der Fehlermeldung
-        if (!passwordsMatch) {
-            errorMessage.textContent = "Die Passwörter stimmen nicht überein.";
-        } else if (!isCheckboxChecked) {
-            errorMessage.textContent = "Bitte akzeptiere die Datenschutzrichtlinie.";
-        } else {
-            errorMessage.textContent = ""; // Fehlermeldung leeren, wenn alles in Ordnung ist
-        }
+async function loadUsers(){
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.error('Loading error:', e);
     }
+}
 
-    // Event-Listener für die Checkbox
-    checkbox.addEventListener('change', toggleSignUpButton);
 
-    // Event-Listener für die Passwort-Inputs
-    passwordInput.addEventListener('input', toggleSignUpButton);
+/**
+ * With this function we disable the button after click and push the Data into the users Array and POST it over the setItem() into the remote storage
+ */
+async function register() {
+    registerBtn.disabled = true;
+    users.push({
+        name: signUpName.value,
+        email: signUpEmail.value,
+        password: signUpPassword.value,
+    });
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+}
+
+
+/**
+ * This is a function to reset the input form in the html code
+ */
+function resetForm() {
+    signUpName.value = '';
+    signUpEmail.value = '';
+    signUpPassword.value = '';
+    signUpPasswordConfirm.value = '';
+    checkboxSignUp.checked = false;
+    registerBtn.disabled = false;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let signUpButton = document.querySelector('.signUpButton');
+    let passwordInput = document.querySelector('input[placeholder="Password"]');
+    let confirmPasswordInput = document.querySelector('input[placeholder="Confirm Password"]');
+    let errorMessage = document.querySelector('.errorMessage');
+    function toggleSignUpButton() {
+        let passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+        signUpButton.disabled = !passwordsMatch;
+        errorMessage.textContent = passwordsMatch ? "" : "Ups! Your password don't match";
+    }
     confirmPasswordInput.addEventListener('input', toggleSignUpButton);
+    toggleSignUpButton(); // Initial aufrufen, um den Button beim Laden der Seite zu deaktivieren
+    errorMessage.textContent = ""; // Leere die Fehlermeldung beim Laden der Seite
 });
