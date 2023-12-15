eecc67b9af166732327230f46754ead89cdf23cf
FredericPae <frederic.paetzold@outlook.com>
2023-12-15 09:32:18 +0100

    added responsive Contact preview

--- js/renderContactCard.js
+++ js/renderContactCard.js
@@ -1,4 +1,14 @@
-function openContactContainer() {
-    let contact = document.getElementById('mainContactDetails');
+function cardWindow() {
+    const cardBg = document.getElementById('mainContactCard_Bg');
+    const layer = document.getElementById('closeWindowLayer');
+    const contact = document.getElementById('mainContactDetails');
+    const mainContactCard = document.getElementById('mainContactContainer');
+    cardBg.classList.toggle('d-none');
+    layer.classList.toggle('d-none');
     contact.classList.toggle('d-none');
+    mainContactCard.classList.toggle('mobile-d-none');
+}
+
+function closeWindow() {
+    cardWindow();
 }
