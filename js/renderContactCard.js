433bd80209f1c08fc36b94b3be2dad6d61b3d8ef
FredericPae <frederic.paetzold@outlook.com>
2023-12-15 11:59:39 +0100

    added mobile screen

--- js/renderContactCard.js
+++ js/renderContactCard.js
@@ -3,10 +3,16 @@ function cardWindow() {
     const layer = document.getElementById('closeWindowLayer');
     const contact = document.getElementById('mainContactDetails');
     const mainContactCard = document.getElementById('mainContactContainer');
+    const arrow = document.getElementById('backArrow');
+    const hideAddBtn = document.getElementById('mobileAddContactBtnContainer');
+    const showSettingBtn = document.getElementById('mobileSettingBtnContainer');
     cardBg.classList.toggle('d-none');
     layer.classList.toggle('d-none');
     contact.classList.toggle('d-none');
     mainContactCard.classList.toggle('mobile-d-none');
+    arrow.classList.toggle('showBackArrow');
+    hideAddBtn.classList.toggle('d-none');
+    showSettingBtn.classList.toggle('mobile-d-none');
 }
 
 function closeWindow() {
