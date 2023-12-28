110de53e982936afd92dca01ffa0b6abc8105d69
DanielSchn <d.schneider@dsc-mail.de>
2023-12-28 11:25:07 +0100

    jsdoc clean

--- js/storage.js
+++ js/storage.js
@@ -2,6 +2,12 @@ const STORAGE_TOKEN = '78PUP18YOP0GTA6TMBG4A7FXS3NWO97PH5OQO23B';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 
+/**
+ * Function to save the Userdata in the remote Storage
+ * @param {string} key - Keyword 'user' in the remote Storage
+ * @param {string} value - Value Data with the stringify JSON Data: email, name, password
+ * @returns 
+ */
 async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
@@ -9,10 +15,14 @@ async function setItem(key, value) {
 }
 
 
+/**
+ * Function to load the Data from the remote Storage with the Key 'user'
+ * @param {string} key - Get the Data from the Storage Keyword 'user'
+ * @returns 
+ */
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
         if (res.data) { 
             return res.data.value;
         } throw `Could not find data with key "${key}".`;
